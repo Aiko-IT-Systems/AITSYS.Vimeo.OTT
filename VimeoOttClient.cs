ceefabe93d286419b8b7931654a3e7c504693c35
Lala Sabathil <aiko@aitsys.dev>
2024-12-12 17:30:24 +0100

    chore: prepare for nuget

---
+++ VimeoOttClient.cs
@@ -0,0 +1,8 @@
+﻿using AITSYS.Vimeo.OTT.Entities;
+
+namespace AITSYS.Vimeo.Ott;
+
+public sealed class VimeoOttClient(VimeoOttConfiguration configuration)
+{
+	internal VimeoOttConfiguration Configuration { get; set; } = configuration;
+}
