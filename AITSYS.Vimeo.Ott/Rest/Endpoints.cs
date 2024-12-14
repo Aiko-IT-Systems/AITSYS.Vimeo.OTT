d19891392e019547c02aa6da390625e61f63b8ac
Lala Sabathil <aiko@aitsys.dev>
2024-12-14 16:16:57 +0100

    add all possible endpoints

--- AITSYS.Vimeo.Ott/Rest/Endpoints.cs
+++ AITSYS.Vimeo.Ott/Rest/Endpoints.cs
@@ -12,7 +12,83 @@ public static class Endpoints
 	/// </summary>
 	public const string BASE_URI = "https://api.vhx.tv";
 
+	/// <summary>
+	///     The customers endpoint.
+	/// </summary>
 	public const string CUSTOMERS = "/customers";
+
+	/// <summary>
+	///     The events endpoint.
+	/// </summary>
 	public const string EVENTS = "/events";
+
+	/// <summary>
+	///     The products endpoint.
+	/// </summary>
 	public const string PRODUCTS = "/products";
+
+	/// <summary>
+	///     The watching endpoint.
+	/// </summary>
+	public const string WATCHING = "/watching";
+
+	/// <summary>
+	///     The watchlist endpoint.
+	/// </summary>
+	public const string WATCHLIST = "/watchlist";
+
+	/// <summary>
+	///     The files endpoint.
+	/// </summary>
+	public const string FILES = "/files";
+
+	/// <summary>
+	///     The sites endpoint.
+	/// </summary>
+	public const string SITES = "/sites";
+
+	/// <summary>
+	///     The authorizations endpoint.
+	/// </summary>
+	public const string AUTHORIZATIONS = "/authorizations";
+
+	/// <summary>
+	///     The browse endpoint.
+	/// </summary>
+	public const string BROWSE = "/browse";
+
+	/// <summary>
+	///     The report endpoint.
+	/// </summary>
+	public const string REPORT = "/report";
+
+	/// <summary>
+	///     The comments endpoint.
+	/// </summary>
+	public const string COMMENTS = "/comments";
+
+	/// <summary>
+	///     The collections endpoint.
+	/// </summary>
+	public const string COLLECTIONS = "/collections";
+
+	/// <summary>
+	///     The update position endpoint.
+	/// </summary>
+	public const string UPDATE_POSITION = "/update_position";
+
+	/// <summary>
+	///     The items endpoint.
+	/// </summary>
+	public const string ITEMS = "/items";
+
+	/// <summary>
+	///     The analytics endpoint.
+	/// </summary>
+	public const string ANALYTICS = "/analytics";
+
+	/// <summary>
+	///     The videos endpoint.
+	/// </summary>
+	public const string VIDEOS = "/videos";
 }
