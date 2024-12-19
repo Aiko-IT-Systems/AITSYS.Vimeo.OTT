36cc1c37390557f4b198af67199f4a83935142c3
Lala Sabathil <aiko@aitsys.dev>
2024-12-19 17:06:05 +0100

    docs: add better docs for the webhook attribute

--- AITSYS.Vimeo.Ott/Attributes/VimeoOttWebhookAttribute.cs
+++ AITSYS.Vimeo.Ott/Attributes/VimeoOttWebhookAttribute.cs
@@ -1,7 +1,19 @@
+using AITSYS.Vimeo.Ott.Entities;
+
 namespace AITSYS.Vimeo.Ott.Attributes;
 
 /// <summary>
 ///     Marks a method to handle one or more topics for incoming OTT webhooks.
+/// <para>You can create a method like this:</para>
+/// <code>
+/// [VimeoOttWebhook("customer.product.created", "customer.product.deleted")]
+/// async Task&lt;ActionResult&gt; HandleVimeoOttWebhookAsync(OttWebhook webhook)
+/// {
+///      // do stuff
+///      return new OkResult();
+/// }
+/// </code>
+/// <para>Important is that you define the parameter <c><see cref="OttWebhook">OttWebhook</see> webhook</c>, since we populate to this parameter.</para>
 /// </summary>
 /// <param name="topics">The topics to handle.</param>
 [AttributeUsage(AttributeTargets.Method)]
