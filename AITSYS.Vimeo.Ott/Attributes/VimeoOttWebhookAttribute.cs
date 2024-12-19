10d9f5538b088e411cdb85544b257bad4490a803
Lala Sabathil <aiko@aitsys.dev>
2024-12-19 15:13:14 +0100

    chore: add missing copyright header

--- AITSYS.Vimeo.Ott/Attributes/VimeoOttWebhookAttribute.cs
+++ AITSYS.Vimeo.Ott/Attributes/VimeoOttWebhookAttribute.cs
@@ -1,3 +1,5 @@
+// Copyright 2025 Aiko IT Systems. See https://github.com/Aiko-IT-Systems/AITSYS.Vimeo.OTT/blob/main/LICENSE.md for the license.
+
 namespace AITSYS.Vimeo.Ott.Attributes;
 
 /// <summary>
